e7535e437a02b4a0268ae3670d329c3c91cef538
Sona Moushigian <snmshgn@aol.com>
2019-01-10 16:37:29 -0500

    Assignment BlocJams Jquery Sliders Complete

---
+++ scripts/helper.js
@@ -0,0 +1,11 @@
+class Helper {
+
+  playPauseAndUpdate (song) {
+    const totalTime = player.getDuration();
+    $('#time-control .total-time').text(totalTime);
+    player.playPause(song);
+  }
+
+}
+
+var helper = new Helper;
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -12,7 +12,7 @@
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('button#previous').on('click', function() {
@@ -23,7 +23,7 @@
     if (previousSongIndex < album.songs[0]) { return; }
 
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
 
   setInterval( () => {
@@ -38,5 +38,9 @@
 
   $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
+  });
+
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
   });
 }
