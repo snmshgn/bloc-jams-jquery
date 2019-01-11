1ab713f26c708c8bad15c7f494d34ec1ef8db78e
Sona Moushigian <snmshgn@aol.com>
2019-01-10 20:01:46 -0500

    missed something

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -4,19 +4,18 @@
       <tr>
         <td>
           <button>
-            <span class="song-number>"${index+1}</span>
+            <span class="song-number">${index + 1}</span>
             <span class="ion-play"></span>
             <span class="ion-pause"></span>
           </button>
         </td>
         <td>${song.title}</td>
         <td>${song.duration}</td>
-      </tr>
+       </tr>
     `);
 
     song.element.on('click', event => {
-      helper.playPauseAndUpdate(song);
-      $('button#play-pause').attr('playState', player.playState);
+      player.playPause(song);
     });
 
     $('#song-list').append(song.element);
